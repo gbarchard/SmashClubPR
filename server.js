b9f725a249339e34a5473d14b5a435478380d88b
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-11-23 12:10:03 -0500

    builds poll across all tournaments

--- server.js
+++ server.js
@@ -5,12 +5,26 @@ var findPlayersInTournament = require('./functions/find-players-in-tournament.js
 var getColleyScores = require('./functions/get-colley-scores.js');
 var createPoll = require('./functions/create-poll.js');
 
-findTournaments( function(tournaments, response) {
-    findTournamentMatches(tournaments[0].tournament.id, function (tournament, response) { //need for each here
-        let tournamentPlayers = findPlayersInTournament(tournament)
-        let scores = getColleyScores(tournament, tournamentPlayers)
-        let poll = createPoll(tournamentPlayers, scores)
+function runPoll(allMatches) {
+        let allPlayers = []
+        allPlayers = findPlayersInTournament(allMatches)
+        let scores = getColleyScores(allMatches, allPlayers)
+        let poll = createPoll(allPlayers, scores)
         console.log(poll)
+}
+
+findTournaments( function(tournaments, response) {
+    var allMatches = []
+    var itemsProcessed = 0
+    tournaments.forEach((tournament, index, array) => {
+        findTournamentMatches(tournament.tournament.id, function (tournament, response) { //need for each here
+            allMatches = allMatches.concat(tournament)
+            itemsProcessed++
+            if (itemsProcessed === array.length) {
+                runPoll(allMatches)
+            }
+        }); 
     })
 })
 
+//https://stackoverflow.com/questions/18983138/callback-after-all-asynchronous-foreach-callbacks-are-completed
