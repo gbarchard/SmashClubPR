e3f5cc287855943dc857e4c49b11715d387d2569
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-11-23 10:14:53 -0500

    get 1 tournament by doing get

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 
 var findTournamentMatches = require('./sources/challonge/tournaments/list-tournament-matches.js');
+var findTournaments = require('./sources/challonge/tournaments/list-tournaments.js');
 var findPlayersInTournament = require('./functions/find-players-in-tournament.js');
 var getColleyScores = require('./functions/get-colley-scores.js');
 var createPoll = require('./functions/create-poll.js');
 
-
-findTournamentMatches(5244080, function (tournament, response) {
-    let tournamentPlayers = findPlayersInTournament(tournament)
-    let scores = getColleyScores(tournament, tournamentPlayers)
-    let poll = createPoll(tournamentPlayers, scores)
-    console.log(poll)
+findTournaments( function(tournaments, response) {
+    findTournamentMatches(tournaments[0].tournament.id, function (tournament, response) { //need for each here
+        let tournamentPlayers = findPlayersInTournament(tournament)
+        let scores = getColleyScores(tournament, tournamentPlayers)
+        let poll = createPoll(tournamentPlayers, scores)
+        console.log(poll)
+    })
 })
+
