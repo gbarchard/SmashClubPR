4a2894eeffbe15f4103a1e69098d4e01bc6ee471
Grant Barchard <gbarchard@c-24-72-228-8.ni.gigamonster.net>
2018-12-19 20:07:40 -0500

    another attempt to fix

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ var Discord = require('discord.io');
 var getPoll = require('./functions/get-poll.js');
 var prArguments = require('./functions/pr-arguments.js');
 
-http.createServer(onRequest).listen(process.env.PORT || 6000)
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 //const config = require("./config.json");
 
