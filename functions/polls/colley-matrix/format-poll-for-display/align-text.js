5adf06b0fbed368d067416a002f9f74767fe9434
Les Barchard <lbarchard@c-24-72-228-77.ni.gigamonster.net>
2018-12-23 12:55:54 -0500

    aligned poll ranking column

--- functions/polls/colley-matrix/format-poll-for-display/align-text.js
+++ functions/polls/colley-matrix/format-poll-for-display/align-text.js
@@ -1,6 +1,7 @@
 var alignText = function alignText(poll) {
 	newPoll = []
 	poll.forEach(player => {
+		player[0] = ' '.repeat(3-player[0].toString().length) + player[0];
 		newPoll.push(player.join(' - '))
 	});
 	newPoll = newPoll.join('\n')
