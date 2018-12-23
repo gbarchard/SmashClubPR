1ca30900d95402ea4abeed5f33c34281e606984f
Grant Barchard <gbarchard@c-24-72-228-8.ni.gigamonster.net>
2018-12-23 15:43:05 -0500

    started getBracket

---
+++ functions/brackets/get-bracket.js
@@ -0,0 +1,6 @@
+var findTournaments = require('../../sources/challonge/tournaments/list-tournaments.js');
+
+var getBracket = function getBracket() {
+    return "hello world"
+}
+module.exports = getBracket
