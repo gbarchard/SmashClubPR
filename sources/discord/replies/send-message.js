4db3cff24036cb74d6cbae378d50775f6bb8b52e
Grant Barchard <gbarchard@c-24-72-228-8.ni.gigamonster.net>
2018-12-22 20:50:29 -0500

    moved replies into discord

---
+++ sources/discord/replies/send-message.js
@@ -0,0 +1,8 @@
+var sendMessage = function sendMessage(bot, channelID, message) {
+    bot.sendMessage({
+        to: channelID,
+        message: message
+        });
+    }
+
+module.exports = sendMessage;
