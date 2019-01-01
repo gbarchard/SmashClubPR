7f3758a6fd7946cf0b0b5ec3b166cfb39b0f74aa
Grant Barchard <gbarchard@c-24-72-228-55.ni.gigamonster.net>
2018-12-31 21:20:33 -0500

    test

--- sources/challonge/functions/list-tournaments-by-start-date.js
+++ sources/challonge/functions/list-tournaments-by-start-date.js
@@ -3,6 +3,8 @@ var findTournaments = require('../tournaments/list-tournaments.js');
 var findTournamentsByStartDate = function findTournamentsByStartDate(startDate,endDate,callback,state) {
    
     findTournaments("2000-01-01","2100-01-01",function(tournaments,response) {
+        console.log(startDate)
+        console.log(endDate)
         var filteredTournaments = []
         tournaments.forEach(tournament => {
             var tournamentStartedAt = tournament.tournament.started_at.substring(0,10)
