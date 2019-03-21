20a7bb3eb2d3a6dfa0e825125260410ed0fb1724
Grant Barchard <gbarchard@c-24-72-228-63.ni.gigamonster.net>
2019-03-21 19:21:01 -0400

    Made Some text look better

--- sources/discord/replies/align-text.js
+++ sources/discord/replies/align-text.js
@@ -1,16 +1,15 @@
 var alignText = function alignText(poll) {
 	newPoll = []
-	maxLengthOfName = 0
-	poll.forEach(player =>  {
-		if (player[1].length > maxLengthOfName) {
-			maxLengthOfName = player[1].length
-		}
-	})
+
 
 	poll.forEach(player => {
 		player[0] = ' '.repeat(2-player[0].toString().length) + player[0];
-		player[1] = player[1] + ' '.repeat(maxLengthOfName-player[1].toString().length);
-		newPoll.push(player.join('-'))
+		player[3] = player[2]
+		player[2] = player[1]
+		player[1] = player[3]
+		player.splice(3)
+		newPoll.push(player.join('|'))
+		
 	});
 	newPoll = newPoll.join('\n')
 
